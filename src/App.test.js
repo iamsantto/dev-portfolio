b0434307ce9e6e297ab266d362e1f009f5f2b191
Santhosh <santhosh@keepworks.com>
2020-09-15 13:42:35 +0530

    added test cases for links

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,20 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './components/App';
 
-test('renders learn react link', () => {
+test('resume link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Resume/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('blog link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Blog/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('pictures link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Pictures/i);
   expect(linkElement).toBeInTheDocument();
 });
